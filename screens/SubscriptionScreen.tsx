3aa1a59f8cc9ded9c2e6b541e5492b5ff523583e
Azamat <smagzanoff@gmail.com>
2024-05-05 23:27:41 +0500

    Subscription Screen update

--- screens/SubscriptionScreen.tsx
+++ screens/SubscriptionScreen.tsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-
+import { Video, ResizeMode } from 'expo-av';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function SubscriptionScreen() {
+
+  const isFocused = useIsFocused();
+  
+
   return (
     <View style={styles.background}>
-    
+  
+      <Video
+        source={require('../assets/background.mp4')}
+        style={StyleSheet.absoluteFillObject}
+        resizeMode={ResizeMode.COVER}
+        shouldPlay={isFocused}
+        isLooping
+      />
+
+   
+      <LinearGradient
+        colors={['rgba(0, 0, 0, 1)', 'transparent']}
+        locations={[0.1, 1]}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        style={styles.topGradient}
+      />
+
+      
+      <LinearGradient
+        colors={['black', 'rgba(0, 0, 0, 1)', 'transparent']}
+        locations={[0, 0.4, 1]}
+        start={{ x: 0.5, y: 1 }}
+        end={{ x: 0.5, y: 0 }}
+        style={styles.bottomGradient}
+      />
+
+     
       <View style={styles.contentContainer}>
         <Text style={styles.heading}>
           Watch 1M+ anime series. {'\n'}
-          No ads, no interruptions.{'\n'}
+          No ads, no interruptions. {'\n'}
           Watch offline.
         </Text>
 
@@ -28,6 +61,20 @@ export default function SubscriptionScreen() {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+  },
+  topGradient: {
+    position: 'absolute', 
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '20%', 
+  },
+  bottomGradient: {
+    position: 'absolute', 
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: '55%', 
   },
   contentContainer: {
     flex: 1,
--- themes/theme.ts
+++ themes/theme.ts
@@ -3,13 +3,13 @@
 import { DefaultTheme, DarkTheme as NavigationDarkTheme } from '@react-navigation/native';
 
 export const darkTheme = {
-  ...NavigationDarkTheme, // Inherit default dark theme properties
+  ...NavigationDarkTheme, 
   colors: {
-    ...NavigationDarkTheme.colors, // Inherit existing color palette
-    background: '#121212', // Dark background
-    primary: '#1DB954', // Accent color
-    card: '#1C1C1C', // Navigation bar background
-    text: '#FFFFFF', // Text color
-    border: '#1F1F1F', // Border color
+    ...NavigationDarkTheme.colors, 
+    background: '#121212', 
+    primary: '#1DB954', 
+    card: '#1C1C1C', 
+    text: '#FFFFFF', 
+    border: '#1F1F1F',
   },
 };
